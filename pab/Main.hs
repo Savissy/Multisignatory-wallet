c76ef4437af2816b69818a2224027c07df35306d
Savissy <111025155+Savissy@users.noreply.github.com>
2025-06-27 07:23:58 -0700

    Update Main.hs

--- pab/Main.hs
+++ pab/Main.hs
@@ -1,107 +1,149 @@
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleContexts   #-}
-{-# LANGUAGE LambdaCase         #-}
-{-# LANGUAGE RankNTypes         #-}
-{-# LANGUAGE TypeApplications   #-}
-{-# LANGUAGE TypeFamilies       #-}
-{-# LANGUAGE TypeOperators      #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
-module Main(main, writeCostingScripts) where
+module Main where
 
-import           Control.Monad                       (void)
-import           Control.Monad.Freer                 (interpret)
-import           Control.Monad.IO.Class              (MonadIO (..))
-import           Data.Aeson                          (FromJSON (..), ToJSON (..), genericToJSON, genericParseJSON
-                                                     , defaultOptions, Options(..))
-import           Data.Default                        (def)
-import qualified Data.OpenApi                        as OpenApi
-import           GHC.Generics                        (Generic)
-import           Plutus.Contract                     (ContractError)
-import           Plutus.PAB.Effects.Contract.Builtin (Builtin, SomeBuiltin (..), BuiltinHandler(contractHandler))
-import qualified Plutus.PAB.Effects.Contract.Builtin as Builtin
-import           Plutus.PAB.Simulator                (SimulatorEffectHandlers)
-import qualified Plutus.PAB.Simulator                as Simulator
-import qualified Plutus.PAB.Webserver.Server         as PAB.Server
-import           Plutus.Contracts.Game               as Game
-import           Plutus.Trace.Emulator.Extract       (writeScriptsTo, ScriptsConfig (..), Command (..))
-import           Prettyprinter                       (Pretty (..), viaShow)
-import           Ledger.Index                        (ValidatorMode(..))
-import qualified Wallet.Emulator.Wallet as Wallet
+import           Control.Monad              (void)
+import           Data.Aeson                 (ToJSON, FromJSON)
+import           GHC.Generics               (Generic)
+import           Ledger
+import           Ledger.TimeSlot            (slotToEndPOSIXTime)
+import           Ledger.Typed.Scripts       (ValidatorTypes(..))
+import           Plutus.Contract
+import           Plutus.Trace.Emulator      as Emulator
+import           Plutus.V2.Ledger.Api       hiding (Validator)
+import qualified Plutus.V2.Ledger.Api       as PlutusV2
+import           PlutusTx
+import           PlutusTx.Prelude           hiding (Semigroup(..), unless)
+import           Prelude                    (IO, Semigroup(..), Show(..), String)
+import qualified Prelude
+
+--------------------------------------------------
+-- Data Types
+--------------------------------------------------
+
+data WalletDatum = WalletDatum
+  { signers  :: [PubKeyHash]
+  , required :: Integer
+  , deadline :: POSIXTime
+  } deriving Show
+
+PlutusTx.unstableMakeIsData ''WalletDatum
+PlutusTx.makeLift ''WalletDatum
+
+newtype WalletRedeemer = Spend [PubKeyHash]
+  deriving Show
+
+PlutusTx.unstableMakeIsData ''WalletRedeemer
+PlutusTx.makeLift ''WalletRedeemer
+
+--------------------------------------------------
+-- Validator
+--------------------------------------------------
+
+{-# INLINABLE mkValidator #-}
+mkValidator :: WalletDatum -> WalletRedeemer -> ScriptContext -> Bool
+mkValidator dat (Spend sigs) ctx =
+  let
+    info = scriptContextTxInfo ctx
+    validSignatures = length (filter (\pkh -> txSignedBy info pkh && pkh `elem` signers dat) sigs)
+    timeOk = contains (from $ deadline dat) (txInfoValidRange info)
+  in
+    traceIfFalse "Not enough signatures" (validSignatures >= required dat) &&
+    traceIfFalse "Deadline not reached" timeOk
+
+data MultiSig
+instance ValidatorTypes MultiSig where
+    type DatumType MultiSig = WalletDatum
+    type RedeemerType MultiSig = WalletRedeemer
+
+typedValidator :: TypedValidator MultiSig
+typedValidator = mkTypedValidator @MultiSig
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = mkUntypedValidator
+
+validator :: Validator
+validator = validatorScript typedValidator
+
+valHash :: ValidatorHash
+valHash = validatorHash typedValidator
+
+scrAddress :: Address
+scrAddress = scriptAddress validator
+
+--------------------------------------------------
+-- Testing in Emulator
+--------------------------------------------------
 
 main :: IO ()
-main = void $ Simulator.runSimulationWith handlers $ do
-    Simulator.logString @(Builtin StarterContracts) "Starting plutus-starter PAB webserver on port 9080. Press enter to exit."
+main = runEmulatorTraceIO testTrace
 
-    (wallet, _paymentPubKeyHash) <- Simulator.addWallet
-    Simulator.waitNSlots 1
-    liftIO $ writeFile "scripts/wallet" (show $ Wallet.getWalletId wallet)
+testTrace :: EmulatorTrace ()
+testTrace = do
+  let wallet1 = knownWallet 1
+      wallet2 = knownWallet 2
+      wallet3 = knownWallet 3
+      wallet4 = knownWallet 4
 
-    shutdown <- PAB.Server.startServerDebug
+  let pkh1 = mockWalletPaymentPubKeyHash wallet1
+      pkh2 = mockWalletPaymentPubKeyHash wallet2
+      pkh3 = mockWalletPaymentPubKeyHash wallet3
 
-    -- Example of spinning up a game instance on startup
-    -- void $ Simulator.activateContract (Wallet 1) GameContract
-    -- You can add simulator actions here:
-    -- Simulator.observableState
-    -- etc.
-    -- That way, the simulation gets to a predefined state and you don't have to
-    -- use the HTTP API for setup.
+  h1 <- activateContractWallet wallet1 endpoints
 
-    -- Pressing enter results in the balances being printed
-    void $ liftIO getLine
+  let datum = WalletDatum
+                { signers  = [unPaymentPubKeyHash pkh1, unPaymentPubKeyHash pkh2, unPaymentPubKeyHash pkh3]
+                , required = 2
+                , deadline = slotToEndPOSIXTime def 5
+                }
 
-    Simulator.logString @(Builtin StarterContracts) "Balances at the end of the simulation"
-    b <- Simulator.currentBalances
-    Simulator.logBalances @(Builtin StarterContracts) b
+      redeemer = Spend [unPaymentPubKeyHash pkh1, unPaymentPubKeyHash pkh2]
 
-    shutdown
+  -- Deposit
+  callEndpoint @"deposit" h1 (datum, Ada.lovelaceValueOf 10_000_000)
 
--- | An example of computing the script size for a particular trace.
--- Read more: <https://plutus.readthedocs.io/en/latest/plutus/howtos/analysing-scripts.html>
-writeCostingScripts :: IO ()
-writeCostingScripts = do
-  let config = ScriptsConfig { scPath = "/tmp/plutus-costing-outputs/", scCommand = cmd }
-      cmd    = Scripts { unappliedValidators = FullyAppliedValidators }
-      -- Note: Here you can use any trace you wish.
-      trace  = correctGuessTrace
-  (totalSize, exBudget) <- writeScriptsTo config "game" trace def
-  putStrLn $ "Total size = " <> show totalSize
-  putStrLn $ "ExBudget = " <> show exBudget
+  void $ Emulator.waitUntilSlot 6
 
+  -- Spend
+  callEndpoint @"spend" h1 (datum, redeemer)
 
-data StarterContracts =
-    GameContract
-    deriving (Eq, Ord, Show, Generic)
-    deriving anyclass OpenApi.ToSchema
+  void $ Emulator.waitNSlots 1
 
--- NOTE: Because 'StarterContracts' only has one constructor, corresponding to
--- the demo 'Game' contract, we kindly ask aeson to still encode it as if it had
--- many; this way we get to see the label of the contract in the API output!
--- If you simple have more contracts, you can just use the anyclass deriving
--- statement on 'StarterContracts' instead:
---
---    `... deriving anyclass (ToJSON, FromJSON)`
-instance ToJSON StarterContracts where
-  toJSON = genericToJSON defaultOptions {
-             tagSingleConstructors = True }
-instance FromJSON StarterContracts where
-  parseJSON = genericParseJSON defaultOptions {
-             tagSingleConstructors = True }
+--------------------------------------------------
+-- Contract endpoints
+--------------------------------------------------
 
-instance Pretty StarterContracts where
-    pretty = viaShow
+type MultiSigSchema =
+        Endpoint "deposit" (WalletDatum, Value)
+    .\/ Endpoint "spend" (WalletDatum, WalletRedeemer)
 
-instance Builtin.HasDefinitions StarterContracts where
-    getDefinitions = [GameContract]
-    getSchema =  \case
-        GameContract -> Builtin.endpointsToSchemas @Game.GameSchema
-    getContract = \case
-        GameContract -> SomeBuiltin (Game.game @ContractError)
+endpoints :: Contract () MultiSigSchema Text ()
+endpoints = awaitPromise (deposit' `select` spend') >> endpoints
+  where
+    deposit' = endpoint @"deposit" $ \(d, v) -> deposit d v
+    spend'   = endpoint @"spend" $ \(d, r) -> spend d r
 
-handlers :: SimulatorEffectHandlers (Builtin StarterContracts)
-handlers =
-    Simulator.mkSimulatorHandlers def
-    $ interpret (contractHandler Builtin.handleBuiltin)
+deposit :: WalletDatum -> Value -> Contract w s Text ()
+deposit datum value = do
+  let tx = mustPayToTheScript datum value
+  void $ submitTxConstraints typedValidator tx
 
+spend :: WalletDatum -> WalletRedeemer -> Contract w s Text ()
+spend datum redeemer = do
+  utxos <- utxosAt scrAddress
+  let orefs = fst <$> Map.toList utxos
+      lookups = Constraints.unspentOutputs utxos <>
+                Constraints.otherScript validator
+      tx :: TxConstraints (RedeemerType MultiSig) (DatumType MultiSig)
+      tx = mconcat [mustSpendScriptOutput oref (Redeemer $ PlutusTx.toBuiltinData redeemer) | oref <- orefs]
+  void $ submitTxConstraintsWith @MultiSig lookups tx
